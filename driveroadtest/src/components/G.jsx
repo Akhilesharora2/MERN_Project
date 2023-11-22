e252a20aa07ab3b2b9019e99a350c611cd8f987e
Akhilesharora2 <akhilesharora002@gmail.com>
2023-11-22 15:27:45 -0500

    Overlays on all

--- driveroadtest/src/components/G.jsx
+++ driveroadtest/src/components/G.jsx
@@ -5,18 +5,33 @@ import Overview from './GComponents/Overview';
 import GTest from './GComponents/GTest';
 const G = ({auth}) => {
     const [toggleTab, setToggleTab] = useState('Overview');
+    const [overlayMenu, setOverlayMenu] = useState(true);
 
     const tabs = (name) => {
         setToggleTab(name);
     }
 
+    const navSwitch = (val) =>{
+        setOverlayMenu(val);
+        if(val){
+            document.getElementById("myNav").style.height = "5%";
+        }else{
+            document.getElementById("myNav").style.height = "100%";
+        }
+    }
   return (
     <div>
         <section id="G">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-4 my-5">
-                    <ul className='mt-5'>
+                    <div className="col-md-4 my-5 G1Contents" id='myNav'>
+                          {/* <!-- Button to close the overlay navigation --> */}
+                          <NavLink href="javascript:void(0)" className={overlayMenu ?"closebtn hidebtn": "closebtn showbtn"} id="closebtn" onClick={() =>navSwitch(true)}>&times;</NavLink>
+
+<NavLink href="javascript:void(0)" className={overlayMenu ? "openbtn showbtn": "openbtn hidebtn"} id="openbtn" onClick={()=>navSwitch(false)}>&#9776; open</NavLink>
+
+
+                    <ul className='mt-5 G1ContentsList'>
                                 <li className={toggleTab === "Overview" ? "display py-1 activeTab" : "display py-1"}>
                                     <NavLink onClick={() => tabs('Overview')}>Overview</NavLink></li>
                                 <li className={toggleTab === "GTest" ? "display py-1 activeTab" : "display py-1"}>
--- driveroadtest/src/components/G2Components/G2Maps.jsx
+++ driveroadtest/src/components/G2Components/G2Maps.jsx
@@ -18,7 +18,7 @@ return(
                 <div className="row">
                     <div className="col">
                         <h5>We are working on this section and will be up and running shortly.</h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184551.80857816365!2d-79.51814416495607!3d43.71840381269127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sin!4v1679207436254!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184551.80857816365!2d-79.51814416495607!3d43.71840381269127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sin!4v1679207436254!5m2!1sen!2sin" width="80%" height="80%" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         {/* // Important! Always set the container height explicitly */}
     {/* <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
